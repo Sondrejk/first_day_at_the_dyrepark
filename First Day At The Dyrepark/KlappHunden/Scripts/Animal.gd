cd3c1fae2210e6556ed889759c5b3b7e86f62b7d
sondrejk <sondrejk@gmail.com>
2023-10-05 14:13:07 +0200

    Score system, begynt å legge inn sprites, game over screen

--- First Day At The Dyrepark/KlappHunden/Scripts/Animal.gd
+++ First Day At The Dyrepark/KlappHunden/Scripts/Animal.gd
@@ -1,21 +1,14 @@
 extends Area2D
 class_name Animal
 
-@onready var game_manager = get_node("/root/Main/KlappHunden")
-
-var start_position = Vector2(-700, 0)
-var is_in_centre : bool = false
-var move_speed : float = 200
+@onready var game_manager : Node2D = get_node("/root/KlappHunden")
+@export var move_speed : float = 200
+var start_position = Vector2(-700, 20)
 
 # When spawned, starts at the starting position
 func _ready():
 	position = start_position
-
-func _process(delta):
-		if walk(delta, 0):
-			if Input.is_key_label_pressed(KEY_I):
-				if walk(delta, 700):
-					despawn()
+	
 
 # Moves the animal across the screen, can give exact x position
 func walk(delta : float, target : float):
--- First Day At The Dyrepark/KlappHunden/Scripts/GameManager.gd
+++ First Day At The Dyrepark/KlappHunden/Scripts/GameManager.gd
@@ -1,14 +1,36 @@
 extends Node2D
 
+
+
 var dog_scene = preload("res://KlappHunden/Scenes/Dog.tscn")
 var bear_scene = preload("res://KlappHunden/Scenes/Bear.tscn")
 
+@onready var game_over_screen : VBoxContainer = $GameOverScreen
+@onready var score_text : Label = $ScoreText
+@onready var game_over_score_text : Label = $GameOverScreen/GameOverScoreText
+
+var is_game_active : bool = true
+var score : int = 0
+
+# Spawns animals randomly based on the timer timeout
 func _on_animal_spawn_timer_timeout():
-	var dog = dog_scene.instantiate()
-	var bear = bear_scene.instantiate()
-	var animals : Array = [dog, bear]
-		
-	add_child(animals[randi_range(0, 1)])
+	if is_game_active:
+		var dog = dog_scene.instantiate()
+		var bear = bear_scene.instantiate()
+		var animals : Array = [dog, bear]
+			
+		add_child(animals[randi_range(0, 1)])
+
+# Adds score and updates the score text
+func add_score(amount):
+	score += amount
+	score_text.text = str("SCORE: ", score)
 
 func game_over():
+	score_text.hide()
+	game_over_score_text.text = score_text.text
+	game_over_screen.show()
+	is_game_active = false
+
+func _on_restart_game_button_down():
 	get_tree().reload_current_scene()
