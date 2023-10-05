2eabf976950e7778e20f3f84af978b7e577286a6
sondrejk <sondrejk@gmail.com>
2023-10-05 10:12:56 +0200

    Revert "trying to make lose condition"
    This reverts commit 6d1a52ca705416efbdbaf38cf400c36b67ef282c.

--- First Day At The Dyrepark/Klapp_Hunden/Animal.gd
+++ First Day At The Dyrepark/Klapp_Hunden/Animal.gd
@@ -4,7 +4,9 @@ class_name Animal
 @export var move_speed : float = 200
 @onready var game_manager : Node2D = get_node("/root/Klapp_hunden")
 var activate_input : bool = false
-var animal_success : bool = false
+var animal_petted : bool = false
+var bear_petted : bool = false
+var animal_not_petted : bool = false
 
 func _ready():
 	position = Vector2(-700, 0)
@@ -13,14 +15,21 @@ func _ready():
 	move_speed *= game_manager.game_speed
 	
 # Makes the animal walk to the centre of the screen
-func walk(delta):
+func walk_on_screen(delta):
+	if position.x <= 0:
 		position.x += move_speed * delta
+		return
+	activate_input = true
+	
+# Makes the animal walk offscreen and then despawn
+func walk_off_screen(delta):
+	if position.x <= 700:
+		position.x += move_speed * delta
+		return
+	despawn()
 	
 func despawn():
 	queue_free()
 
 func _on_area_entered(area):
-	game_manager.game_over()
-
-func wrong_pet():
-	game_manager.game_over()
+	get_tree().reload_current_scene()
