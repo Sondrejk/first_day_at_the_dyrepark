822e4bf814ab055441e37550368077aa14fb2a04
sondrejk <sondrejk@gmail.com>
2023-10-31 08:44:50 +0100

    Added jumping to ReddRudolf

--- First Day At The Dyrepark/ReddRudolf/ReddRudolfPlayer.gd
+++ First Day At The Dyrepark/ReddRudolf/ReddRudolfPlayer.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-var jump_velocity : float = 100
-var gravity : float = 20
+var jump_velocity : float = 600
+var gravity : float = 1000
 
 func _process(delta):
-	if Input.is_action_pressed("up"):
+	
+	# Jumping and gravity
+	if Input.is_action_pressed("up") and is_on_floor():
 		velocity.y -= jump_velocity
 	velocity.y += gravity * delta
+	move_and_slide()
