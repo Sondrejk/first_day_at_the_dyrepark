6550c696ad6a8fe5f503682e1598e965355a0e82
sondrejk <sondrejk@gmail.com>
2023-11-02 09:33:12 +0100

    Worked on ReddRudolf

--- First Day At The Dyrepark/ReddRudolf/ReddRudolfManager.gd
+++ First Day At The Dyrepark/ReddRudolf/ReddRudolfManager.gd
@@ -6,15 +6,40 @@ extends Node2D
 # References to scenes
 var obstacle_scene = preload("res://ReddRudolf/ReddRudolfObstacle.tscn")
 
+# References to nodes
+@onready var score_text : Label = $ScoreText
+@onready var player : CharacterBody2D = $ReddRudolfPlayer
+@onready var game_over_screen = $GameOverScreen
+@onready var game_over_score_text : Label = $GameOverScreen/GameOverScoreText
+@onready var spawn_timer : Timer = $ObstacleSpawnTimer
+
+# Variables
+var score : int = 0
+var game_time : float = 0
+var game_speed : float = 1
+
 func _on_obstacle_spawn_timer_timeout():
 	var obstacle_instance = obstacle_scene.instantiate()
 	add_child(obstacle_instance)
 
+func _process(delta):
+	game_time += delta
+	game_speed = 0.02 * game_time + 1
+	spawn_timer.wait_time = 75/(game_time + 25)
 
-func _on_quit_to_dyrehage_button_down():
-	get_tree().change_scene_to_file(dyrehage_scene)
+func addscore(num):
+	score += num
+	score_text.text = str("SCORE: ", score)
+
+func game_over():
+	player.hide()
+	score_text.hide()
+	game_over_score_text.text = score_text.text
+	spawn_timer.stop()
+	game_over_screen.show()
 
 func _on_restart_game_button_down():
 	get_tree().reload_current_scene()
 
-
+func _on_quit_to_dyrehage_button_down():
+	get_tree().change_scene_to_file(dyrehage_scene)
--- First Day At The Dyrepark/ReddRudolf/ReddRudolfObstacle.gd
+++ First Day At The Dyrepark/ReddRudolf/ReddRudolfObstacle.gd
@@ -1,21 +1,26 @@
 extends Area2D
 
 var start_position : Vector2 = Vector2(800, 17)
-var move_speed : float = 500
+var start_move_speed : float = 500
+var move_speed : float = start_move_speed
 var given_score : bool = false
 
 @onready var player = $"../ReddRudolfPlayer"
 @onready var game_manager = $".."
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position = start_position
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	move_speed = start_move_speed * game_manager.game_speed
+	
 	position.x -= move_speed * delta
-	if global_position.x <= player.position.x:
+	if global_position.x <= player.position.x and not given_score:
 		given_score = true
 		game_manager.addscore(1)
+
+func _on_body_entered(_body):
+	given_score = true # Because then the obstacle won't give a score after death
+	hide()
+	game_manager.game_over()
